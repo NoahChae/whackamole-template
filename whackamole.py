17fd374ee6b93379a27ca76b54527a1546f647e9
Noah Chae <noahjchae@gmail.com>
2024-11-17 21:31:01 -0500

    fixed mole moving off screen

--- whackamole.py
+++ whackamole.py
@@ -1,14 +1,9 @@
 import pygame
 import random
-
-
-
 def main():
     try:
-
         pygame.init()
         molecoords = (0, 0)
-        # You can draw the mole with this snippet:
 
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
@@ -21,7 +16,7 @@ def main():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     event.pos = pygame.mouse.get_pos()
                     if event.pos[0] in range(molecoords[0]+32) and event.pos[1] in range(molecoords[1]+32):
-                        molecoords = (random.randint(0,20)*32, random.randint(0,16)*32)
+                        molecoords = (random.randrange(0,20)*32, random.randrange(0,16)*32)
                         screen.blit(mole_image, mole_image.get_rect(topleft=(molecoords)))
 
             screen.fill("light green")
